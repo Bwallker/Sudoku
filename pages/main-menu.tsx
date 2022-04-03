2d4da165ebc7294f096c10146d3e52024298ae79
bwallker <benbwall@gmail.com>
2022-04-04 02:15:52 +0300

    init

---
+++ pages/main-menu.tsx
@@ -0,0 +1,9 @@
+const MainMenu = () => {
+  return (
+    <div className='row row-cols-1 text-center'>
+      <h1 className='my-5 p-5 col-lg-6 mx-auto'>I am main menu</h1>
+    </div>
+  );
+};
+
+export default MainMenu;
